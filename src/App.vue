f0079c6decc1bda9e454b94139c7ba817d24ec1d
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-06-18 20:45:33 -0300

    18/06 32:21 do curso

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpense />
+    <TransactionList :transactions="transactions"/>
+    <AddTransaction />
+  </div>
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+import { ref } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: 'Flower', amount: -19.99 },
+  { id: 2, text: 'Salary', amount: 19.99 },
+  { id: 3, text: 'Book', amount: -19.99 },
+  { id: 4, text: 'Camera', amount: 19.99 },
+]);
+</script>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'" v-for="transaction in transactions" v-bind:key="transaction.id">
+      {{ transaction.text }} <span>$ {{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue'
+
+  const props = defineProps({
+    transactions: {
+      type: Array,
+      required: true,
+    }
+  });
+</script>
